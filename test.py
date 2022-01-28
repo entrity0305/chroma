faa3f8391d00c741104b063ecc63cc19d3a61452
jaeyun_cha <locha0305@gmail.com>
2022-01-29 00:21:16 +0900

    test.py minor change

--- test.py
+++ test.py
@@ -6,25 +6,18 @@ code = '''
 #this function checks if integer N is a prime number
 function is_prime(N) {
     var k = 2;
-
     while k < N 
     {
         if N % k == 0 {
             return 0;
         }
-
         k = k + 1;
     }
-
     return 1;
 }
-
-
 var is_this_number_prime = is_prime(13);
-
 var x = 1;
 x = 2;
-
 print("Hello, world!");
 '''
 
